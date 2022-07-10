71c96dc37c85029d316ce19b1db40a0c127b6050
ayyem <ayyem@proton.me>
2022-07-10 11:26:54 -0500

    Started ORK status project.

---
+++ Assets/Gaming Is Love/Makinom 2/Plugins/SaveGameCompression/GZipCompression.cs
@@ -0,0 +1,101 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace GamingIsLove.Makinom.Plugins
+{
+	public class GZipCompression : ICompressionHandler
+	{
+		public GZipCompression()
+		{
+
+		}
+
+		public string CompressString(string text)
+		{
+			MemoryStream memoryStrea = null;
+			GZipOutputStream gzipStream = null;
+			string result;
+			try
+			{
+				memoryStrea = new MemoryStream();
+				System.Int32 size = text.Length;
+
+				using(BinaryWriter writer = new BinaryWriter(memoryStrea, Encoding.UTF8))
+				{
+					writer.Write(size);
+
+					gzipStream = new GZipOutputStream(memoryStrea);
+					gzipStream.Write(Encoding.UTF8.GetBytes(text), 0, text.Length);
+
+					gzipStream.Close();
+					result = System.Convert.ToBase64String(memoryStrea.ToArray());
+					memoryStrea.Close();
+
+					writer.Close();
+				}
+			}
+			catch(System.Exception)
+			{
+				result = text;
+			}
+			finally
+			{
+				if(gzipStream != null)
+				{
+					gzipStream.Dispose();
+				}
+				if(memoryStrea != null)
+				{
+					memoryStrea.Dispose();
+				}
+			}
+			return result;
+		}
+
+		public string DecompressString(string text)
+		{
+			string result;
+			MemoryStream memoryStream = null;
+			GZipInputStream gzipStream = null;
+			try
+			{
+				memoryStream = new MemoryStream(System.Convert.FromBase64String(text));
+
+				using(BinaryReader reader = new BinaryReader(memoryStream, Encoding.UTF8))
+				{
+					System.Int32 size = reader.ReadInt32();
+
+					gzipStream = new GZipInputStream(memoryStream);
+					byte[] bytesUncompressed = new byte[size];
+					gzipStream.Read(bytesUncompressed, 0, bytesUncompressed.Length);
+					gzipStream.Close();
+					memoryStream.Close();
+
+					result = Encoding.UTF8.GetString(bytesUncompressed);
+
+					reader.Close();
+				}
+			}
+			catch(System.Exception)
+			{
+				result = text;
+			}
+			finally
+			{
+				if(gzipStream != null)
+				{
+					gzipStream.Dispose();
+				}
+				if(memoryStream != null)
+				{
+					memoryStream.Dispose();
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ Assets/Gaming Is Love/Makinom 2/Plugins/SaveGameCompression/SaveGameCompressionPlugin.cs
@@ -0,0 +1,75 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace GamingIsLove.Makinom.Plugins
+{
+	[EditorSettingInfo("Save Game Compression", "Compress save games.")]
+	public class SaveGameCompressionPlugin : BasePlugin
+	{
+		[EditorHelp("Compress Save Games", "Save games will be compressed.", "")]
+		[EditorLabel("It's recommended to not use encryption when using compression.")]
+		[EditorFoldout("Compression Settings", "The settings for save game compression.", "")]
+		[EditorEndFoldout]
+		public bool compressSaveGames = false;
+
+		public SaveGameCompressionPlugin()
+		{
+
+		}
+
+		public override string Version
+		{
+			get { return "1.0.0"; }
+		}
+
+
+		/*
+		============================================================================
+		Makinom callback functions
+		============================================================================
+		*/
+		public override void OnInitialize()
+		{
+			if(this.compressSaveGames)
+			{
+				Maki.SaveGame.SetCompression(new GZipCompression());
+			}
+		}
+
+		public override void OnNewGame()
+		{
+
+		}
+
+		public override void OnLoadGame()
+		{
+
+		}
+
+		public override void Tick()
+		{
+
+		}
+
+		public override void GUITick()
+		{
+
+		}
+
+		public override void SceneLoaded()
+		{
+
+		}
+
+		public override void SceneNameLoaded(string sceneName)
+		{
+
+		}
+
+		public override bool Call(string info)
+		{
+			return false;
+		}
+	}
+}
